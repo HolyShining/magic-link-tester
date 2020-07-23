e2810b00590d99003829f0bc795447f22315de39
holyshining <holyshine@yahoo.com>
2020-07-23 20:23:59 +0300

    reformat decorators package with Black

--- project/decorators/magic_token_required.py
+++ project/decorators/magic_token_required.py
@@ -9,10 +9,10 @@ from project.models import Tokens
 def magic_token_required(f):
     @functools.wraps(f)
     def decorated_function(*args, **kwargs):
-        token = Tokens.query.filter_by(id=session['token_id']).first()
+        token = Tokens.query.filter_by(id=session["token_id"]).first()
         if token.is_valid is False:
             logout_user()
-            return redirect(url_for('main.index'))
+            return redirect(url_for("main.index"))
         return f(*args, **kwargs)
 
     return login_required(decorated_function)
